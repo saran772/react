cfc2dede70e9d14331dacf2a6926fd216122ed69
saran772 <110741339+saran772@users.noreply.github.com>
2025-04-08 19:14:22 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+/*why we use npm -- because react is so powerful and we have to minimize bundle as well as optimize app
+and those helper packages come inside npm*/
+
+//caret^ is use so that we have updated version if we dont use thn it is stick to given version
+
+//here package-lock tell us that which version we r actually using(it will actually lock the  version)
+
+
