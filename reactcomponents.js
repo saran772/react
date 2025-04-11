f7fcd65ad82055b0becde0807d22d2a7fd192a44
saran772 <110741339+saran772@users.noreply.github.com>
2025-04-12 00:15:43 +0530

    Add files via upload

---
+++ reactcomponents.js
@@ -0,0 +1,15 @@
+//learning components
+//a fun returning react element is functional components
+
+// const Heading=()=>{
+//     return <h1>namaste react</h1>
+// }
+
+// const Headcomp=() =>{
+//     return(
+//        <div>
+//            {1+2+3}
+//        <h1>
+//            namaste react ji</h1>
+//            <h2>saran</h2>
+//            </div>);}
